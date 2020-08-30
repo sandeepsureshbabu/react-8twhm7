cba050a1ce2921599a72b7efa66484c039ede757
sandeepsureshbabu <sandeepsureshbabu.s@gmail.com>
2020-08-30 08:06:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-8twhm7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-8twhm7)
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAqatCtt3jSRGECqDRzaHRIPDtQN5bwFwY",
+  authDomain: "wonder-chat-26e4c.firebaseapp.com",
+  databaseURL: "https://wonder-chat-26e4c.firebaseio.com",
+  projectId: "wonder-chat-26e4c",
+  storageBucket: "wonder-chat-26e4c.appspot.com",
+  messagingSenderId: "861869641366",
+  appId: "1:861869641366:web:25e9daa0e64c6c00c5bcb7",
+  measurementId: "G-GF582CS4XV"
+};
